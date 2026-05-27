9a5685c0af98dadda30a3a448c520d02f40b9665
Alexis Guerville <sadzeih@gmail.com>
2026-05-28 01:32:49 +0200

    fix syntax

--- vars.nix
+++ vars.nix
@@ -1,3 +1,3 @@
 {
-  sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBEkJRsSexxtcUvHtIgiKpgHatFVph5eV4s2zKxQaqEN sadzeih@Jupiter"
+  sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBEkJRsSexxtcUvHtIgiKpgHatFVph5eV4s2zKxQaqEN sadzeih@Jupiter";
 }
